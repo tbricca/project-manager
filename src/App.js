27c7b98fac6ac7267af67f2245ca0260482f5a2a
tbricca <tbricca@gmail.com>
2018-02-08 14:47:40 -0800

    got projects file linked up with main app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects';
 import './App.css';
+// Only need app.css imported on the main folder
+
 
 // App.js is the gateway to our main React Component
  
@@ -9,6 +12,7 @@ class App extends Component {
       // when you return it has to all be in one element (one top level div)
       <div className="App">
        My Project Manager App
+       <Projects />
       </div>
     );
   }
---
+++ src/Components/Projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+// App.js is the gateway to our main React Component
+
+class Projects extends Component {
+    render() {
+        return (
+            // when you return it has to all be in one element (one top level div)
+            <div className="Projects">
+                My Projects 
+      </div>
+        );
+    }
+}
+
+export default Projects;
