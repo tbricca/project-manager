42ed61aeed886cacf81cdb0d4404e00ecf9908a6
tbricca <tbricca@gmail.com>
2018-02-10 10:39:40 -0800

    mapped through the different categories so that user can select it for their project

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -4,9 +4,15 @@ import React, { Component } from 'react';
 
 
 class AddProject extends Component {
+    static defaultProps = {
+        categories: ['Web Design', 'Web Development', 'Mobile Development']
+    }
+
 
     render() {
-        
+        let categoryOptions = this.props.categories.map(category => {
+            return <option key = {category} value = {category}>{category}</option>
+        });
         return (
             // when you return it has to all be in one element (one top level div)
             <div>
@@ -16,6 +22,13 @@ class AddProject extends Component {
                         <label>Title</label><br />
                         <input type="text" ref="title" />
                     </div>
+                    <div>
+                        <label>Category</label><br />
+                        <select ref="category">
+                        {categoryOptions}
+                        </select>
+                    </div>
+
                 </form>
             </div>
         );
