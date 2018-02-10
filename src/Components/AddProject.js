4bf5f7ba935871af4115a45b463311f0b727acff
tbricca <tbricca@gmail.com>
2018-02-10 10:48:42 -0800

    working on be able to submit the new project, added button and starting to get info passd through onto the console

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -4,10 +4,22 @@ import React, { Component } from 'react';
 
 
 class AddProject extends Component {
+    // To store the data that we submit into the state
+    constructor(){
+        super();
+        this.state = {
+            newProject: {}
+        }
+    }
     static defaultProps = {
         categories: ['Web Design', 'Web Development', 'Mobile Development']
     }
-
+    // for the submit button or else it will submit it and we won't be able to see it on the console
+    handleSubmit(e){
+        // console.log('Submitted');
+        console.log(this.refs.title.value);
+        e.preventDefault();
+    }
 
     render() {
         let categoryOptions = this.props.categories.map(category => {
@@ -17,7 +29,7 @@ class AddProject extends Component {
             // when you return it has to all be in one element (one top level div)
             <div>
                 <h3>Add Project</h3>
-                <form>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <div>
                         <label>Title</label><br />
                         <input type="text" ref="title" />
@@ -28,7 +40,7 @@ class AddProject extends Component {
                         {categoryOptions}
                         </select>
                     </div>
-
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
         );
